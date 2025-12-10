556ae7c4b87a524543804b7687a73ad9bfe6d123
Fadi Kaba <fadi.cloud.kaba@gmail.com>
2025-12-10 13:58:25 +1100

    Completing the Channel and Mutexes code

--- cmd/main.go
+++ cmd/main.go
@@ -4,27 +4,29 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/fadikaba81/channelandmutexes/tree/main/internal/result"
-	"github.com/fadikaba81/channelandmutexes/tree/main/internal/worker"
+	"github.com/fadikaba81/channelandmutexes/internal/result"
+	"github.com/fadikaba81/channelandmutexes/internal/worker"
 )
 
 func main() {
 
 	job := make(chan int)
 	var wg sync.WaitGroup
+	var i int
 
 	result := result.New()
 
-	for i := 0; i < 3; i++ {
+	for i = 0; i < 3; i++ {
 		wg.Add(1)
 		go worker.Worker(i, job, result, &wg)
 	}
 
-	for i := 0; i < 10; i++ {
+	for i = range 10 {
+		fmt.Println("Sending job", i)
 		job <- i
 	}
 	close(job)
 	wg.Wait()
 
-	fmt.Println("Final Results:", result.GetAll())
+	fmt.Println("Final Results:", result.GetValue())
 }
